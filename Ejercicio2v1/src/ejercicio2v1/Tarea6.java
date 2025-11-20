5b6cc74e2a1f865841bde422751e7f4c028105f1
JosePedro228 <josepedrodossantosbarro@gmail.com>
2025-11-20 17:26:53 +0100

    ff

---
+++ Ejercicio2v1/src/ejercicio2v1/Tarea6.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio2v1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAM113
+ */
+public class Tarea6 {
+        public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ponga el radio :");
+        float radio = sc.nextFloat();
+        System.out.println("La Longetude 2es :"+(radio*2));
+        System.out.println("La area:"+(Math.PI*radio*radio));
+        }   
+}
